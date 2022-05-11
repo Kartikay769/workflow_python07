619f1c8c74a896d15f262912e4958de24255a334
unknown <kartiklohia7@gmail.com>
2022-05-11 14:47:58 +0530

    this is my new1  python workflow

--- python1.py
+++ python1.py
@@ -1,14 +1,17 @@
-#fibonacci Series
+#even odd odd_elements
+list1 = [3, 6, 9, 12, 15, 18, 21]
+list2 = [4, 8, 12, 16, 20, 24, 28]
+res = list()
 
-n = int(input("Enter the value of 'n': "))
-a = 0
-b = 1
-sum = 0
-count = 1
-print("Fibonacci Series: ", end = " ")
-while(count <= n):
-  print(sum, end = " ")
-  count += 1
-  a = b
-  b = sum
-  sum = a + b
+odd_elements = list1[1::2]
+print("Element at odd-index positions from list one")
+print(odd_elements)
+
+even_elements = list2[0::2]
+print("Element at even-index positions from list two")
+print(even_elements)
+
+print("Printing Final third list")
+res.extend(odd_elements)
+res.extend(even_elements)
+print(res)
